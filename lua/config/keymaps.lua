f3448f8003078384cea92e1ac8e16eea6fde95b6
goodbye-isaac <goodbyeisaac1727@gmail.com>
2025-06-06 21:18:57 +0900

    H Lをバッファの移動から本来のカーソル移動に戻した、バッファ移動は[b,]b

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "<C-l>", "<Esc>")  -- 挿入モードで Ctrl+l を Esc に
+
+-- Hキーで画面の最上部に移動
+vim.keymap.set('n', 'H', 'H', { noremap = true, silent = true })
+
+-- Lキーで画面の最下部に移動
+vim.keymap.set('n', 'L', 'L', { noremap = true, silent = true })
+
